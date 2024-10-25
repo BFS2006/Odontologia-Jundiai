2b266cdbc1a3c7528bbf32dc77a7e75ad79094bb
Bryan Felipe Da Silva <bryanfelipedasilva7@gmail.com>
2024-10-25 17:17:59 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// const slides = document.querySelector('.slides');
+// const slide = document.querySelectorAll('.slide');
+// let currentIndex = 0;
+
+// function showSlide(index) {
+//     const totalSlides = slide.length;
+//     currentIndex = (index + totalSlides) % totalSlides; // Garante que o índice fique dentro do limite
+//     slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
+
+// // Chama a função autoSlide a cada 2 segundos
+// setInterval(() => {
+//     showSlide(currentIndex + 1);
+// }, 2000);
+
+// // Inicia com a primeira imagem visível
+// showSlide(currentIndex);
